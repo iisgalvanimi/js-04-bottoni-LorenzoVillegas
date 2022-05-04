474a9cdcadef0d55e299186621bcbf3f25f41be3
LorenzoVillegas <villegas.lorenzo@iisgalvanimi.edu.it>
2022-05-04 09:45:20 +0200

    commit 2

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const btns_nodeLst = document.querySelectorAll(".tab-btn");
+const btns_arr = Array.from(btns_nodeLst)
+
+
+function evidenziaBottone (e) {
+    console.log("Evidenzio bottone " + e.target.id)
+
+    for (let i = 0; i < btns_arr.lenght; i++) {
+	    btns_arr[i].classList.remove("activeButtonClass");
+    }
+    e.target.classList.add("activeButtonClass");
+}
+
+btns_arr[0].addEventListener("click", evidenziaBottone);
+btns_arr[1].addEventListener("click", evidenziaBottone);
+btns_arr[2].addEventListener("click", evidenziaBottone);
